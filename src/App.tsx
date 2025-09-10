53b7982f3e3a31be514847692a5bc3f4695cc4f0
nathanvalignat1 <nathan.valignat1@gmail.com>
2025-09-10 11:30:24 +0200

    chore: bootstrap Vite + Tailwind v4 + tooling

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="p-8">
+        <h1 className="text-3xl font-bold text-sky-600">HomeHub 1.1</h1>
+        <p className="mt-2 text-gray-700">Tailwind est actif 🎉</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
